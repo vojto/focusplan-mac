bbd3442e2340dfd52b995ff4026c2d040bf13499
Vojtech Rinik <vojto@rinik.net>
2017-04-25 15:30:23 +0200

    Adding the Core Data setup code

--- FocusPlan/AppDelegate.swift
+++ FocusPlan/AppDelegate.swift
@@ -19,6 +19,85 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "FocusPlan")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unresolved error \(error)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: Core Data Saving and Undo support
+    
+    @IBAction func saveAction(_ sender: AnyObject?) {
+        let context = persistentContainer.viewContext
+        
+        if !context.commitEditing() {
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
+        }
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Customize this code block to include application-specific recovery steps.
+                let nserror = error as NSError
+                NSApplication.shared().presentError(nserror)
+            }
+        }
+    }
+    
+    func windowWillReturnUndoManager(window: NSWindow) -> UndoManager? {
+        return persistentContainer.viewContext.undoManager
+    }
+    
+    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplicationTerminateReply {
+        let context = persistentContainer.viewContext
+        
+        if !context.commitEditing() {
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
+            return .terminateCancel
+        }
+        
+        if !context.hasChanges {
+            return .terminateNow
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            
+            // Customize this code block to include application-specific recovery steps.
+            let result = sender.presentError(nserror)
+            if (result) {
+                return .terminateCancel
+            }
+            
+            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
+            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
+            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
+            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
+            let alert = NSAlert()
+            alert.messageText = question
+            alert.informativeText = info
+            alert.addButton(withTitle: quitButton)
+            alert.addButton(withTitle: cancelButton)
+            
+            let answer = alert.runModal()
+            if answer == NSAlertSecondButtonReturn {
+                return .terminateCancel
+            }
+        }
+
+        return .terminateNow
+    }
 
 }
 
