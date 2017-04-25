d913d9b69bf9c2f9fcdf196dbbbe7d55e2a3635f
Vojtech Rinik <vojto@rinik.net>
2017-04-25 07:57:38 +0200

    Initial Commit

---
+++ FocusPlan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FocusPlan
+//
+//  Created by Vojtech Rinik on 4/25/17.
+//  Copyright © 2017 Median. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
