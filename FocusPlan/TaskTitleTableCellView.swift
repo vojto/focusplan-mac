df071c2e1e6b3e1a1707632a871f7243a5721a33
Vojtech Rinik <vojto@rinik.net>
2017-04-28 19:10:01 +0200

    Strikethrough

--- FocusPlan/TaskTitleTableCellView.swift
+++ FocusPlan/TaskTitleTableCellView.swift
@@ -26,46 +26,56 @@ class TaskTitleTableCellView: EditableTableCellView {
         let isPlanned = task.producer.pick({ $0.reactive.isPlanned.producer })
         let project = task.producer.pick({ $0.reactive.project.producer })
         
-        if let field = textField {
-            field.reactive.stringValue <~ task.producer.pick({ $0.reactive.title.producer }).map { $0 ?? "" }
+        guard let field = textField else { return assertionFailure() }
+        
+        let title = task.producer.pick({ $0.reactive.title.producer }).map { $0 ?? "" }
+        
+        let attributedTitle = SignalProducer.combineLatest(title, isFinished).map { title, isFinished -> NSAttributedString in
+            var attributes = [String: Any]()
             
-            isFinished.startWithValues { finished in
-                self.superview?.alpha = (finished ?? false) ? 0.4 : 1
+            if (isFinished ?? false) {
+                attributes[NSStrikethroughStyleAttributeName] = NSUnderlineStyle.styleSingle.rawValue
             }
             
-            SignalProducer.combineLatest(isEditing.producer, isPlanned.producer, project.producer)
-                .startWithValues { editing, planned, project in
-                    let normal = NSFont.systemFont(ofSize: 14, weight: NSFontWeightRegular)
-                    let medium = NSFont.systemFont(ofSize: 14, weight: NSFontWeightMedium)
+            return NSAttributedString(string: title, attributes: attributes)
+        }
+
+        field.reactive.attributedStringValue <~ attributedTitle
+        
+        SignalProducer.combineLatest(isEditing.producer, isPlanned.producer, project.producer)
+            .startWithValues { editing, planned, project in
+                let normal = NSFont.systemFont(ofSize: 14, weight: NSFontWeightRegular)
+                let medium = NSFont.systemFont(ofSize: 14, weight: NSFontWeightMedium)
+                
+                /*
+                if editing {
+                    field.font = normal
+                    field.textColor = NSColor.labelColor
+                } else if (planned ?? false) && self.wantsHighlightPlanned {
+                    field.font = medium
                     
-                    /*
-                    if editing {
-                        field.font = normal
-                        field.textColor = NSColor.labelColor
-                    } else if (planned ?? false) && self.wantsHighlightPlanned {
-                        field.font = medium
-                        
-                        if let color = project?.color,
-                            let nsColor = Palette.decode(colorName: color) {
-                            field.textColor = nsColor
-                        } else {
-                            field.textColor = NSColor.labelColor
-                        }
+                    if let color = project?.color,
+                        let nsColor = Palette.decode(colorName: color) {
+                        field.textColor = nsColor
                     } else {
-                        field.font = normal
                         field.textColor = NSColor.labelColor
                     }
-                    */
-            }
+                } else {
+                    field.font = normal
+                    field.textColor = NSColor.labelColor
+                }
+                */
         }
         
-        if let button = finishedButton {
-            button.reactive.image <~ isFinished.map { finished -> NSImage in
-                if (finished ?? false) {
-                    return #imageLiteral(resourceName: "TaskChecked")
-                } else {
-                    return #imageLiteral(resourceName: "TaskUnchecked")
-                }
+        
+        guard let button = finishedButton else { return assertionFailure() }
+    
+
+        button.reactive.image <~ isFinished.map { finished -> NSImage in
+            if (finished ?? false) {
+                return #imageLiteral(resourceName: "TaskChecked")
+            } else {
+                return #imageLiteral(resourceName: "TaskUnchecked")
             }
         }
     }
