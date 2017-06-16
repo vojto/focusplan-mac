9b9c8bb1f9c4c7d85b0cadde8e470a95bb42d3ba
Vojtech Rinik <vojto@rinik.net>
2017-06-16 07:12:37 -0400

    Moving some of the UI code out to NiceKit

--- FocusPlan/NSOutlineView+Extensions.swift
+++ FocusPlan/NSOutlineView+Extensions.swift
@@ -6,16 +6,4 @@
 //  Copyright © 2017 Median. All rights reserved.
 //
 
-import Foundation
-import AppKit
 
-extension NSOutlineView {
-    func edit(at row: Int, column: Int) {
-        if row == -1 {
-            return
-        }
-        
-        guard let view = self.view(atColumn: column, row: row, makeIfNecessary: false) as? EditableTableCellView else { return }
-        view.startEditing()
-    }
-}
