336ff0666bfbfdbb37e2db6872ba54b92c72b1da
Vojtech Rinik <vojto@rinik.net>
2017-09-07 07:39:18 +0200

    Adding the buttons for switching weeks

--- FocusPlan/CalendarHeaderView.swift
+++ FocusPlan/CalendarHeaderView.swift
@@ -30,13 +30,23 @@ class CalendarHeaderView: NSView {
     let daysStack = NSStackView()
 
     func setup() {
+        setupHeader()
+        setupBorder()
+        setupDaysStack()
+        setupButtons()
+    }
+
+    func setupHeader() {
         let label = HeaderLabel(text: "This week")
         label.addToHeaderRow(view: self)
+    }
 
-
+    func setupBorder() {
         let border = BorderView()
         border.bottom(self)
+    }
 
+    func setupDaysStack() {
         addSubview(daysStack)
         constrain(daysStack) { view in
             view.left == view.superview!.left + CalendarCollectionLayout.kWeeklySideMargin
@@ -46,7 +56,29 @@ class CalendarHeaderView: NSView {
 
         daysStack.orientation = .horizontal
         daysStack.distribution = .fillEqually
+    }
 
+    func setupButtons() {
+
+        let prevImage = NSImage(named: NSImageNameGoLeftTemplate)!
+        let nextImage = NSImage(named: NSImageNameGoRightTemplate)!
+
+        let prevButton = NSButton(image: prevImage, target: nil, action: nil)
+        let todayButton = NSButton(title: "Today", target: nil, action: nil)
+        let nextButton = NSButton(image: nextImage, target: nil, action: nil)
+
+        let buttons = [prevButton, todayButton, nextButton]
+
+        let stack = NSStackView(views: buttons)
+        stack.orientation = .horizontal
+        stack.spacing = 1.0
+
+        addSubview(stack)
+
+        constrain(stack) { buttons in
+            buttons.right == buttons.superview!.right - 20.0
+            buttons.bottom == buttons.superview!.bottom - 45.0
+        }
     }
 
     func update() {
